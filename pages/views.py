4d81aa7404bbc8db690653d67bd3dbda2acf678a
Alexis <alexisanchez@csumb.edu>
2021-04-21 05:19:56 +0000

    Updating pages...

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+    return HttpResponse("<title>ILoveWeVote</title><h1>Hello World, my name is Alexis Sanchez. This is my Django Project. </h1> <br> I'm building this project in preparation of an internship with a wonderful company called WeVote You can click here to find them at WeVote.us")
+    
